26bdf3c782c8e2d8ca39cd4018060aae9cf713ad
Royce Santo <roycesanto@yahoo.com>
2019-11-02 14:31:49 +0800

    WIP FIring system

---
+++ Assets/Scripts/Player/FiringSystem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FiringSystem : MonoBehaviour
+{
+    #region Testing
+    public int maxAmmo;
+    public int ammoRemaining;
+
+    public int reloadAmount;
+    #endregion Testing
+
+
+    public void Reload()
+    {
+        ammoRemaining += reloadAmount;
+        ammoRemaining = (ammoRemaining > maxAmmo) ? maxAmmo : ammoRemaining; 
+    }
+
+    public void Fire()
+    {
+        if(true) // Check if in hitbox
+        {
+            // Damage
+        }
+    }
+
+    public void Damage(/*Reference target*/)
+    {
+        // Damages target
+    }
+}
