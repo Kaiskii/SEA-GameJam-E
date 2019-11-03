cfaa550428237dd7f9368538bd564f089910b504
Royce Santo <roycesanto@yahoo.com>
2019-11-03 18:29:58 +0800

    Pause Game

---
+++ Assets/Scripts/AnimatorEvent/PauseGameController.cs
@@ -0,0 +1,21 @@
+﻿using CarrotEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseGameController : MonoBehaviour
+{
+    GameManager gameManager { get { return Toolbox.Instance.FindManager<GameManager>(); } }
+
+    public void ContinueGame()
+    {
+        gameManager.ContinueGame();
+    }
+
+    public void QuitGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
